1d364923767b19179124640170bb3d9c41df5cca
Olav <olavslo@stud.ntnu.no>
2023-10-17 21:40:56 +0200

    feat: Real-time speech to text from mic input.

---
+++ RealTimeSpeechToText/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+class Program 
+{
+    async static Task FromMic(SpeechConfig speechConfig)
+    {
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        Console.WriteLine("Speak into your microphone.");
+        var result = await speechRecognizer.RecognizeOnceAsync();
+        Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+    }
+
+    async static Task Main(string[] args)
+    {
+        var speechConfig = SpeechConfig.FromSubscription("REPLACE_WITH_SUBSCRIPTION_KEY", "REPLACE_WITH_REGION");
+        await FromMic(speechConfig);
+    }
+}
